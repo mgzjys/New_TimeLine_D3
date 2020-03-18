24b8e97d755ee3cc4c1eb1417f55731d502902d2
Yao <mgzjys@gmail.com>
2020-03-18 08:24:20 -0400

    Update mapa_new.js

--- map/mapa_new.js
+++ map/mapa_new.js
@@ -97,10 +97,11 @@ var mapCan = d3
   .append("svg")
   .attr("width", wCan)
   .attr("height", hCan);
+
+d3.select("#year").html(trimestres[trimestres.length - 1].substring(0, 4));
 d3.select("#monthday").html(
   trimestres[trimestres.length - 1].substring(5)
 );
-d3.select("#year").html(trimestres[trimestres.length - 1].substring(0, 4));
 
 var height = 330,
   width = 885,
